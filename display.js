91f0a984ddc0e5d524996d9e01d9e5578588129b
naveen arebailu <naveen.arebail@gmail.com>
2017-09-02 14:09:33 +0530

    files added

---
+++ display.js
@@ -0,0 +1,5 @@
+// consume interface and function from namespace 
+// declare the message with interface datatype;
+var messageValue = { type: "Here I am accessing namespace method and interface" };
+// call the namespace method with parameter as interface property;
+message.displayMessage(messageValue.type);
